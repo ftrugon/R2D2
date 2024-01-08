91c2641191d930298b699b7b758dfc2700ccd399
ftrugon <fjtrujillo@iesrafaelalberti.es>
2024-01-08 20:22:08 +0100

    version 1

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,20 @@
+var posX = 0
+var posY = 0
+
+
+fun mover(listamoverse: List<Int>){
+    
+}
+
+fun main(){
+    val listacosas = listOf(
+    listOf(10, 5, -2),
+    listOf(0, 0, 0),
+    listOf(),
+    listOf(-10, -5, 2),
+    listOf(-10, -5, 2, 4, -8)
+    )
+    for (lista in listacosas){
+        mover(lista)
+    }
+}
